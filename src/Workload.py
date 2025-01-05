5f4c22ef1870297ba5ff32c1c637b4febb1a071f
fortrest <ars.sobolev@gmail.com>
2025-01-05 17:45:04 +0500

    define calculation function

---
+++ src/Workload.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class Workload(Enum):
+    NORMAL = 1
+    MODERATE = 2
+    HIGH = 3
+    VERY_HIGH = 4
--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,10 @@
-def main() -> None:
+from src.Workload import Workload
+
+INITIAL_COST = 0
+
+def calculate_delivery_cost(distance: float, size: float, fragile: bool, workload: Workload) -> int:
     pass
 
 
 if __name__ == '__main__':
-    main()
+    calculate_delivery_cost(1.1, 1.1, True, Workload.MODERATE)
