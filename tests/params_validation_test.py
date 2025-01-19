18fe58d8f68c97328ff377c2ff3a0c31f8cc7c9c
fortrest <ars.sobolev@gmail.com>
2025-01-19 22:34:20 +0500

    add tests for raises in params validation

---
+++ tests/params_validation_test.py
@@ -0,0 +1,64 @@
+import pytest
+
+from src.Config import MAX_FRAGILE_DISTANCE
+from src.Errors import NEGATIVE_DISTANCE_ERROR, NEGATIVE_SIZE_ERROR, DISTANCE_EXCEED_WITH_FRAGILE_ERROR
+from src.__main__ import validate_params
+
+
+@pytest.fixture()
+def default_params() -> dict[str, int | bool]:
+    return {
+        'distance': 1,
+        'size': 1,
+        'fragile': False
+    }
+
+
+@pytest.mark.parametrize('negative_distance',[
+    -0.1,
+    -1,
+    -100
+])
+def test_negative_distance_raises_exception(default_params, negative_distance) -> None:
+    params = {
+        **default_params,
+        'distance': negative_distance
+    }
+
+    expected_message = NEGATIVE_DISTANCE_ERROR.format(value=negative_distance)
+    with pytest.raises(ValueError, match=expected_message):
+        validate_params(**params)
+
+
+@pytest.mark.parametrize('negative_size',[
+    -0.1,
+    -1,
+    -100
+])
+def test_negative_size_raises_exception(default_params, negative_size) -> None:
+    params = {
+        **default_params,
+        'size': negative_size
+    }
+
+    expected_message = NEGATIVE_SIZE_ERROR.format(value=negative_size)
+    with pytest.raises(ValueError, match=expected_message):
+        validate_params(**params)
+
+
+@pytest.mark.parametrize('distance_exceedance', [
+    0.1,
+    1,
+    100
+])
+def test_big_distance_with_fragile_raises_exception(default_params, distance_exceedance) -> None:
+    exceed_distance = MAX_FRAGILE_DISTANCE + distance_exceedance
+    params = {
+        **default_params,
+        'fragile': True,
+        'distance': exceed_distance
+    }
+
+    expected_message = DISTANCE_EXCEED_WITH_FRAGILE_ERROR.format(value=exceed_distance, max_distance=MAX_FRAGILE_DISTANCE)
+    with pytest.raises(ValueError, match=expected_message):
+        validate_params(**params)
